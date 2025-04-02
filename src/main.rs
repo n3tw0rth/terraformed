bd555f33bcda93e7434ac7334911d84a0798d2c4
n3tw0rth <rtheekshanafernando@gmail.com>
2025-04-02 20:23:41 +0530

    chore(tui): template from ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use terraformed::app::App;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal).await;
+    ratatui::restore();
+    result
 }
---
+++ src/tui/ui.rs
@@ -0,0 +1,31 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::{Color, Stylize},
+    widgets::{Block, BorderType, Paragraph, Widget},
+};
+
+use crate::app::App;
+
+impl Widget for &App {
+    /// Renders the user interface widgets.
+    ///
+    // This is where you add new widgets.
+    // See the following resources:
+    // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
+    // - https://github.com/ratatui/ratatui/tree/master/examples
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let block = Block::bordered()
+            .title("terraformed")
+            .title_alignment(Alignment::Center)
+            .border_type(BorderType::Rounded);
+        let text = format!("< {} >", self.counter);
+        let paragraph = Paragraph::new(text)
+            .block(block)
+            .fg(Color::Cyan)
+            .bg(Color::Black)
+            .centered();
+
+        paragraph.render(area, buf);
+    }
+}
