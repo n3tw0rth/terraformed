64f3386f88824ff1c536699095ce4c57b012e2b0
Ushira Dineth <ushiradineth@gmail.com>
2025-04-28 09:26:18 +0530

    Add Skeleton UI based on initial design (#2)
    * feat(ui): add skeleton UI
    
    * docs: add current UI preview
    
    * feat: add task file
    
    * chore: ignore .DS_Store on macos

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,7 +1,6 @@
 use ratatui::{
     buffer::Buffer,
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
-    style::{Color, Stylize},
+    layout::{Constraint, Layout, Rect},
     text::Line,
     widgets::{Block, BorderType, Paragraph, Widget},
 };
@@ -9,25 +8,65 @@ use ratatui::{
 use crate::app::App;
 
 impl Widget for &App {
-    /// Renders the user interface widgets.
-    ///
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let chunks = Layout::new(
-            Direction::Horizontal,
-            [Constraint::Percentage(75), Constraint::Percentage(25)],
-        )
-        .vertical_margin(0)
-        .split(area);
+        let [body, footer] = Layout::vertical([
+            Constraint::Min(0), // Takes whatever space is left
+            Constraint::Length(3),
+        ])
+        .areas(area);
+
+        let [body_left_sector, body_right_sector] =
+            Layout::horizontal([Constraint::Percentage(20), Constraint::Percentage(80)])
+                .areas(body);
+
+        let [keybinds] = Layout::horizontal([Constraint::Percentage(100)]).areas(footer);
+
+        let [project, runs, workspaces] = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(0), // Takes whatever space is left
+            Constraint::Percentage(30),
+        ])
+        .areas(body_left_sector);
+
+        let [tabs, command_log] =
+            Layout::vertical([Constraint::Percentage(70), Constraint::Percentage(30)])
+                .areas(body_right_sector);
 
         {
-            let left_area = chunks[0];
-            let paragraph = Paragraph::new("LEFT").centered().block(Block::bordered());
-            paragraph.render(left_area, buf);
+            Paragraph::new("Project name -> Workspace")
+                .centered()
+                .block(Block::bordered().border_type(BorderType::Rounded))
+                .render(project, buf);
         }
         {
-            let right_area = chunks[1];
-            let paragraph = Paragraph::new("RIGHT").centered().block(Block::bordered());
-            paragraph.render(right_area, buf);
+            Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title("Runs")
+                .render(runs, buf);
+        }
+        {
+            Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title("Workspaces")
+                .render(workspaces, buf);
+        }
+        {
+            Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title("Diff")
+                .render(tabs, buf);
+        }
+        {
+            Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title("Command log")
+                .render(command_log, buf);
+        }
+        {
+            let text = Line::from(
+                "Apply: a | Plan: p | Destroy: d | State: s | Workspace: w | Keybinds: ? | Cancel: q",
+            );
+            Paragraph::new(text).left_aligned().render(keybinds, buf);
         }
     }
 }
