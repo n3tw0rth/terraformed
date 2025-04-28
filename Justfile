b495ecfde9ef39ab0be5ecec986d1ea3d5b680a3
Ushira Dineth <ushiradineth@gmail.com>
2025-04-28 09:28:40 +0530

    Add Tracing and CLI (#3)
    * feat(ui): add skeleton UI
    
    * docs: add current UI preview
    
    * feat: add task file
    
    * chore: ignore .DS_Store on macos
    
    * feat(tracing): add runtime logs
    
    * feat(cli): add clap and log level customization
    
    ---------
    
    Co-authored-by: 0xbyt3z <40062006+n3tw0rth@users.noreply.github.com>

--- Justfile
+++ Justfile
@@ -1,6 +1,11 @@
+run:
+    cargo run
 watch:
     cargo watch -x run -w src
 
 fmt:
   cargo fmt --all
   cargo clippy --all-features -- -D warnings
+
+log:
+    tail -n 10 -f terraformed.log
---
+++ src/tracing.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+
+use color_eyre::eyre::Result;
+use tracing::level_filters::LevelFilter;
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{self, layer::SubscriberExt, util::SubscriberInitExt, Layer};
+
+pub struct Tracer;
+impl Tracer {
+    pub fn init(level_filter: LevelFilter) -> Result<()> {
+        // TODO: Get the data directory from CLI Args or Environment Variables
+        let directory = PathBuf::from(".");
+        std::fs::create_dir_all(directory.clone())?;
+
+        // INFO: terraformed.log
+        let log_path = directory.join(format!("{}.log", env!("CARGO_PKG_NAME")).clone());
+        let log_file = std::fs::File::create(log_path)?;
+
+        let file_subscriber = tracing_subscriber::fmt::layer()
+            .with_file(true)
+            .with_line_number(true)
+            .with_writer(log_file)
+            .with_target(false)
+            .with_ansi(false)
+            .with_filter(level_filter);
+
+        tracing_subscriber::registry()
+            .with(file_subscriber)
+            .with(ErrorLayer::default())
+            .init();
+
+        Ok(())
+    }
+}
